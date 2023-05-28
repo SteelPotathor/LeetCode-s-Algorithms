f71b0a669532b6078504766b012ef71574f5bf0d
SteelPotato <riouslain@gmail.com>
2023-05-28 10:17:25 +0200

    Training

---
+++ Two Pointers/Trapping Rain Water.py
@@ -0,0 +1,22 @@
+def trap(height):
+
+    if not height:
+        return 0
+
+    l, r = 0, len(height) - 1
+    maxL, maxR = height[l], height[r]
+    res = 0
+
+    while l < r:
+        if maxL > maxR:
+            r -= 1
+            maxR = max(height[r], maxR)
+            res += maxR - height[r]
+        else:
+            l += 1
+            maxL = max(height[l], maxL)
+            res += maxL - height[l]
+    return res
+
+
+print(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
