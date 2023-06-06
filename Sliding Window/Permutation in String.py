d24d4925b0e3a39dabf23922d6c03e6d5cd7a729
SteelPotato <riouslain@gmail.com>
2023-06-06 14:04:48 +0200

    Training

--- Sliding Window/Permutation in String.py
+++ Sliding Window/Permutation in String.py
@@ -2,28 +2,31 @@ def checkInclusion(s1, s2):
     if len(s1) > len(s2):
         return False
 
-    map, res = {}, {}
+    counts1, counts2 = [0] * 26, [0] * 26
+    for i in range(len(s1)):
+        counts1[ord(s1[i]) - ord("a")] += 1
+        counts2[ord(s2[i]) - ord("a")] += 1
 
-    for c in s1:
-        map[c] = map.get(c, 0) + 1
+    matches = 0
+    for m in range(26):
+        if counts1[m] == counts2[m]:
+            matches += 1
 
-    l, r, cnt = 0, 0, 0
-    while r < len(s2) and map != res:
-        if s2[r] in map:
-            res[s2[r]] = res.get(s2[r], 0) + 1
-            cnt += 1
-            r += 1
-            if cnt == len(s1) + 1:
-                cnt -= 1
-                if res[s2[l]] <= 1:
-                    res.pop(s2[l])
-                else:
-                    res[s2[l]] -= 1
-                l += 1
-        else:
-            r += 1
-            l = r
-            cnt = 0
-            res.clear()
-        print(res, cnt, len(s1))
-    return map == res
+    l, r = 0, len(s1)
+    while matches < 26 and r < len(s2):
+        idxR = ord(s2[r]) - ord("a")
+        counts2[idxR] += 1
+        if counts2[idxR] == counts1[idxR]:
+            matches += 1
+        elif counts2[idxR] - 1 == counts1[idxR]:
+            matches -= 1
+
+        idxL = ord(s2[l]) - ord("a")
+        counts2[idxL] -= 1
+        if counts2[idxL] == counts1[idxL]:
+            matches += 1
+        elif counts2[idxL] + 1 == counts1[idxL]:
+            matches -= 1
+        l += 1
+        r += 1
+    return matches == 26
